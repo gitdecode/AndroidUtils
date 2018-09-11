21321187cdce276d11ab89fbf55e0025d6d1fc36
tsj <374013107@qq.com>
2018-09-11 13:10:28 +0800

    move SDCardUtils to FileUtils

--- src/com/github/androidutils/MainActivity.java
+++ src/com/github/androidutils/MainActivity.java
@@ -2,8 +2,6 @@ package com.github.androidutils;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
